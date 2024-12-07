5955763eecb2cf946003b4329590fae013c03839
modi vinay <modi.vinay02@gmail.com>
2024-12-07 19:39:20 +0530

    first commit
    first date 7-12-2024

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './companents/Sidebar/Sidebar'
+import Main from './companents/Main/Main'
+
+const App = () => {
+  return (
+    <>
+     <Sidebar/> 
+     <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ src/config/zenay.js
@@ -0,0 +1,46 @@
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyDEWc6plckBk8f_TsNuDK3MDFlQIGLx1_M";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-pro",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+   // safetySettings: Adjust safety settings
+   // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response=result.response
+    console.log(response.text());
+
+    return response.text()
+
+  }
+  
+  export default run;
